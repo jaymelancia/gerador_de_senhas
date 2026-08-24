093460c66ed9d809c7ffc11e0178043f8b0942d7
jaymelancia <laura.bonzanini.vieira@escola.pr.gov.br>
2026-08-24 12:17:25 +0000

    24.08/2

--- main.js
+++ main.js
@@ -78,3 +78,7 @@ function classificaSenha(tamanhoAlfabeto){
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
+const unicodeEspecial = '©®™✓✿⚡🚀💎★';
+if (checkboxUnicode.checked) {
+    alfabeto += unicodeEspecial;
+}
